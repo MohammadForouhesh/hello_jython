64f6109baf594e49871eed03da5a81ff59f93031
Mohammad H Forouhesh <Mohammad.Forouhesh@outlook.com>
2016-12-12 12:18:11 +0330

    firts commit

---
+++ hello.py
@@ -0,0 +1,17 @@
+from javax.swing import *
+
+
+def hello(event):
+    print "Hello. I'm an event."
+
+
+def test():
+    frame = JFrame("Hello Jython")
+    button = JButton("Hello",font=("Currier", 100, 60), actionPerformed=hello)
+    frame.add(button)
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+    frame.setSize(300, 300)
+    frame.show()
+
+
+test()
